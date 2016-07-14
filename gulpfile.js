5e88270045c9861b7a36546cd328acac5973650f
Christoffer Jahren <christoffer@jahren.as>
2016-07-14 15:39:10 +0200

    added gulp

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp');
+var bump = require('gulp-bump');
+
+gulp.task('bump', function() {
+  gulp.src('package.json')
+    .pipe(bump({
+      type: 'patch'
+    }))
+    .pipe(gulp.dest('./'))
+})
